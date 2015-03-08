01694b14ee6c2cd240c534c473a25e28a6dbec57
Johan Wikman <johan.wikman@iki.fi>
2015-03-08 21:19:59 +0200

    Cleanup
    Now uses smart-pointer and std::make_shared. All ifs and the
    following statement on separate lines; allows for the placement
    of a breakpoint if the condition is triggered.

--- playercomputer.cpp
+++ playercomputer.cpp
@@ -1,52 +1,67 @@
 #include "playercomputer.h"
-#include "player.h"
+#include <cassert>
 #include "ea/ea.h"
 
-EA *ea;
+using namespace std;
 
-
-PlayerComputer::PlayerComputer(string str) : Player(str)
+PlayerComputer::PlayerComputer(const string& str)
+    : Player(str)
 {
 }
 
 PlayerComputer::~PlayerComputer()
 {
-    delete ea;
 }
 
-void PlayerComputer::attached_to_game(Game * g, int position, bool rotate_to_south)
+void PlayerComputer::attached_to_game(Game* pGame, int position, bool rotate_to_south)
 {
-    my_game = g;
+    my_game = pGame;
     my_position = position;
     my_rotate_to_south = rotate_to_south;
+
     //ea = new EA(g, this,
     //            "T:\\_home\\SchachtnerTh\\RulesFiles\\Bidnet76Clean.txt",
     //            "T:\\_home\\SchachtnerTh\\RulesFiles\\PidroBidRulesAI_Master013v1.txt");
     char* pDir = getenv("PIDRO_AI_DIR");
-    string dir(pDir ? pDir : "C:");
 
-    ea = new EA(g, this, dir + "\\Bidnet76Clean.txt", dir + "\\PidroBidRulesAI_Master013v1.txt");
+    string dir(pDir ? pDir : "C:");
+    string rulesFile(dir + "\\Bidnet76Clean.txt");
+    string bidRulesFile(dir + "\\PidroBidRulesAI_Master013v1.txt");
+
+    sEa = make_shared<EA>(pGame, this, rulesFile, bidRulesFile);
 }
 
-int PlayerComputer::give_bid(int minimum){
-
+int PlayerComputer::give_bid(int minimum)
+{
     // minimum is the currect higest bid
     // if 0 or -1  the next highest bid is 6
     // if minimum is 14, the player can exceed this with a bid of 14
 
-    ea->getBidHint();
+    assert(sEa.get());
+    sEa->getBidHint();
 
-    if (minimum ==5) {return 6;}
+    if (minimum == 5) {
+        return 6;
+    }
 
     int rnd = rand() % 10;
-    if (rnd < 5) {return 0;}     //  50% to pass
+    if (rnd < 5) {
+        return 0;
+    }     //  50% to pass
 
-    if (minimum ==0) {return 10;}
-    if (minimum ==14) {return 14;}
-    return minimum+1;
+    if (minimum == 0) {
+        return 10;
+    }
+
+    if (minimum == 14) {
+        return 14;
+    }
+
+    return minimum + 1;
 }
 
-int PlayerComputer::give_color(){
+int PlayerComputer::give_color()
+{
     // color can be 0 ..3
     int rnd = rand() % 4;
 
--- playercomputer.h
+++ playercomputer.h
@@ -1,20 +1,28 @@
 #ifndef PLAYERCOMPUTER_H
 #define PLAYERCOMPUTER_H
+
+#include <memory>
 #include "player.h"
 
+class EA;
 
 class PlayerComputer : public Player
 {
 public:
     PlayerComputer();
-    PlayerComputer(string str);
+    PlayerComputer(const string& str);
+    PlayerComputer(const PlayerComputer&) = delete;
+    PlayerComputer& operator = (const PlayerComputer&) = delete;
     ~PlayerComputer();
+
     int give_bid(int minimum);
     int give_color();
+
     // this is a callback function and is called from the game after succesful registration
-    void attached_to_game(Game * g, int position, bool rotate_to_south);
+    void attached_to_game(Game* pGame, int position, bool rotate_to_south);
 
-
+private:
+    std::shared_ptr<EA> sEa;
 };
 
 #endif // PLAYERCOMPUTER_H
